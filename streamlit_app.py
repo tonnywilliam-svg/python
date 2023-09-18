5f7a2a3ac5a56df542b3d36144b25bd412c1e700
tonnywilliam-svg <63226542+tonnywilliam-svg@users.noreply.github.com>
2023-09-18 15:15:11 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 if __name__ == '__main__':
     df = pd.read_csv("countries.csv")
     df = df.drop(df.columns[0],axis=1)
-    # df['amount']=df['amount'].str.replace('\(male.+$','',regex=True)
+    # df['amount']=df['amount'].str.replace('(male','\n (male')
     table = pd.pivot(df, values='amount', index=['item', 'sub-item'], columns='country')
     st.dataframe(table, use_container_width=True)
 
