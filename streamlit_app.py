cb8134dc242404ca36f8063c8bfb9ee40df66b73
tonnywilliam-svg <63226542+tonnywilliam-svg@users.noreply.github.com>
2023-09-17 20:53:25 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,6 @@ import streamlit as st
 import pandas as pd
 
 if __name__ == '__main__':
-    data = pd.read_csv("countries.csv")
-    st.table(data)
+    df = pd.read_csv("countries.csv")
+    df = df.drop(df.columns[0],axis=1)
+    st.table(df)
