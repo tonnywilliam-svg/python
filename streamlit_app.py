3718a9e432a31f66f75d781cdba30e4b95cdf355
tonnywilliam-svg <63226542+tonnywilliam-svg@users.noreply.github.com>
2023-09-18 10:50:25 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
-from st_aggrid import AgGrid
 import streamlit as st
 import pandas as pd
 
@@ -6,6 +5,5 @@ if __name__ == '__main__':
     df = pd.read_csv("countries.csv")
     df = df.drop(df.columns[0],axis=1)
     table = pd.pivot(df, values='amount', index=['item', 'sub-item'], columns='country')
-    # st.table(df)
-    # st.write(table)
-    AgGrid(table)
+    st.write(table)
+
