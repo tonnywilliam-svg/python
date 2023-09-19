fece7299c9349dc7f8cb9f2c776a38c253bc6850
tonnywilliam-svg <63226542+tonnywilliam-svg@users.noreply.github.com>
2023-09-18 17:21:37 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,17 @@
 import streamlit as st
 import pandas as pd
 
+countries=['china', 'russia', 'korea-south', 'united-states']
+options = st.multiselect(
+    'Pick your countries',
+    countries,default=countries)
+
 if __name__ == '__main__':
-    df = pd.read_csv("countries.csv")
-    df = df.drop(df.columns[0],axis=1)
-    df['amount']=df['amount'].str.replace('\(male.+$','',regex=True)
+    df0 = pd.read_csv('countries.csv')
+    df0 = df0.drop(df0.columns[0],axis=1)
+    df=df0.query("country in @options")
+    df['amount']=df['amount'].str.replace('(male ','<br> m:')
+    df['amount']=df['amount'].str.replace(r"/female (.+)\)", r"<br> f:\1", regex=True)
     table = pd.pivot(df, values='amount', index=['item', 'sub-item'], columns='country')
-    st.dataframe(table, use_container_width=True)
+    st.markdown(table.to_html(escape=False), unsafe_allow_html=True)
 
